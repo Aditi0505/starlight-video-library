b4feaad13c94995024605393ac79cfa5bc51e166
Aditi0505 <aditi5may1997@gmail.com>
2022-05-23 16:40:33 +0530

    feat: add redux store

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Mockman from "mockman-js";
 import { Route, Routes } from "react-router-dom";
 import { Footer, NavBar, PageNotFound } from "./components";
+import { Home, Videos } from "./redux";
 
 const App = () => {
   return (
@@ -8,6 +9,8 @@ const App = () => {
       <NavBar />
       <Routes>
         <Route path="*" element={<PageNotFound />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/feed" element={<Videos />} />
         <Route path="/mockman" element={<Mockman />} />
       </Routes>
       <Footer />
