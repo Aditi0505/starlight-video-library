2c1cb97739892629f2e66a1a2ed748ecd3fa2c55
Aditi0505 <aditi5may1997@gmail.com>
2022-05-26 01:55:04 +0530

    feat:add theme to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Mockman from "mockman-js";
+import { useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import { Footer, NavBar, PageNotFound } from "./components";
-import { Home, SingleVideo, Videos } from "./redux";
+import { Footer, NavBar, PageNotFound, RequiresAuth } from "./components";
+import { Home, Playlist, SingleVideo, Videos, Login, Signup } from "./redux";
 
 const App = () => {
+  const { theme } = useSelector((store) => store.auth);
   return (
-    <div>
+    <div className={theme === "light" ? "light-mode" : "dark-mode"}>
       <ToastContainer className="toastify-container" />
       <NavBar />
       <Routes>
@@ -14,6 +16,8 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/feed" element={<Videos />} />
         <Route path="/video/:videoId" element={<SingleVideo />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/mockman" element={<Mockman />} />
       </Routes>
       <Footer />
