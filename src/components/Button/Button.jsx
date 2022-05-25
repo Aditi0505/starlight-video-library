425db68f92207af8c8b17106896e459312e8f784
Aditi0505 <aditi5may1997@gmail.com>
2022-05-26 01:54:25 +0530

    feat:add theme

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,11 @@
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { logoutUser } from "../../redux/features/auth/authSlice";
 const Button = ({ buttonState, route, handleClick }) => {
+  const dispatch = useDispatch();
+  const logoutUserHandler = () => {
+    dispatch(logoutUser());
+  };
   return (
     <li>
       {buttonState === "Watch Now" ? (
@@ -9,6 +15,12 @@ const Button = ({ buttonState, route, handleClick }) => {
           onClick={() => handleClick("All")}
         >
           <button className="btn btn-ghost-primary">{buttonState}</button>
+        </Link>
+      ) : buttonState === "Logout" ? (
+        <Link to={`/${route}`} className="nav-icon">
+          <button className="btn btn-ghost-primary" onClick={logoutUserHandler}>
+            {buttonState}
+          </button>
         </Link>
       ) : (
         <Link to={`/${route}`} className="nav-icon">
