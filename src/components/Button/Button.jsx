2ec1eb1c430ef70d26380bfe82a8de1ec326ba20
Aditi0505 <aditi5may1997@gmail.com>
2022-05-26 11:42:28 +0530

    add: logout toast

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,12 @@
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 import { logoutUser } from "../../redux/features/auth/authSlice";
 const Button = ({ buttonState, route, handleClick }) => {
   const dispatch = useDispatch();
   const logoutUserHandler = () => {
     dispatch(logoutUser());
+    toast.success("User logged out!");
   };
   return (
     <li>
