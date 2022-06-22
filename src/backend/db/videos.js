8ab75821fec7f915d336c0acab0ff4ed0424ad4d
Aditi0505 <aditi5may1997@gmail.com>
2022-06-23 01:55:16 +0530

    add: addition of more dummy data

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,88 +5,46 @@
 
 export const videos = [
   {
-    _id: "RsEZmictANA",
-    alt: "willow-music-video",
-    views: "103M",
-    duration: "4:13",
-    title: "Willow",
-    thumbnail: "/assets/gif/willow.webp",
+    _id: "SVY8I46dkb0",
+    alt: "AMA-concert-video",
+    views: "83M",
+    duration: "11:40",
+    title: "Taylor Swift - Live at the 2019 American Music Awards",
+    thumbnail: "/assets/gif/jingle.webp",
     videoBy: "Taylor Swift",
     avatar:
       "https://yt3.ggpht.com/MqKm9xyjonzkICKA78ir0AM-WUR47ntkBeJlgHeIk_rUnPuukiWtzOEmU7UjO8cFoPrBatCh3As=s88-c-k-c0x00ffffff-no-rj",
-    category: "Music Video",
+    category: "Concert",
     description:
-      "Official music video by Taylor Swift performing “willow” - off her evermore album.",
+      "Taylor Swift performs at the 2019 American Music Awards (November 24th, 2019)",
   },
   {
-    _id: "5UMCrq-bBCg",
-    alt: "I Bet You Think About Me-music-video",
-    views: "38M",
-    duration: "4:15",
-    title: "I Bet You Think About Me",
-    thumbnail: "/assets/gif/ibetyou.webp",
-    videoBy: "Taylor Swift",
+    _id: "XnbCSboujF4",
+    alt: "taylor-swift-on-vogue-show",
+    views: "35M",
+    duration: "9:42",
+    title: "73 Questions With Taylor Swift | Vogue",
+    thumbnail: "/assets/gif/vogue.webp",
+    videoBy: "Vogue",
     avatar:
-      "https://yt3.ggpht.com/MqKm9xyjonzkICKA78ir0AM-WUR47ntkBeJlgHeIk_rUnPuukiWtzOEmU7UjO8cFoPrBatCh3As=s88-c-k-c0x00ffffff-no-rj",
-    category: "Music Video",
+      "https://yt3.ggpht.com/thnzkriwUD98vyhpBpL9VWKVC8S172K84W8VOPwrdUOmXpadThVRv6RRnlIgS3CRpnRjGz6ZOw=s48-c-k-c0x00ffffff-no-rj",
+    category: "Interview",
     description:
-      "Music video by Taylor Swift performing I Bet You Think About Me (Taylor's Version) (From The Vault).",
+      "Vogue's May cover star Taylor Swift takes us on a tour of her Beverly Hills home and answers all of our questions, including what she'd be doing if she wasn't a singer, and her best advice for her 19-year-old self. ",
   },
   {
-    _id: "-BjZmE2gtdo",
-    alt: "Lover-music-video",
-    views: "189M",
-    duration: "3:59",
-    title: "Lover",
-    thumbnail: "/assets/gif/lover.webp",
-    videoBy: "Taylor Swift",
+    _id: "FvVnP8G6ITs",
+    alt: "taylor-swift-npr-performance",
+    views: "10M",
+    duration: "28:59",
+    title: "Taylor Swift- NPR Music Tiny Desk Concert",
+    thumbnail: "/assets/gif/npr.webp",
+    videoBy: "NPR Music",
     avatar:
-      "https://yt3.ggpht.com/MqKm9xyjonzkICKA78ir0AM-WUR47ntkBeJlgHeIk_rUnPuukiWtzOEmU7UjO8cFoPrBatCh3As=s88-c-k-c0x00ffffff-no-rj",
-    category: "Music Video",
+      "https://yt3.ggpht.com/ytc/AKedOLTsXk5nEOKKabk8BITs9DYeYR09vM0vORF7c37bEA=s88-c-k-c0x00ffffff-no-rj",
+    category: "Concert",
     description:
-      "Music video by Taylor Swift performing  “lover” - off her lover album.",
-  },
-  {
-    _id: "e-ORhEE9VVg",
-    alt: "BlankSpace-music-video",
-    views: "2.9B",
-    duration: "4:33",
-    title: "Blank Space",
-    thumbnail: "/assets/gif/blankspace.webp",
-    videoBy: "Taylor Swift",
-    avatar:
-      "https://yt3.ggpht.com/MqKm9xyjonzkICKA78ir0AM-WUR47ntkBeJlgHeIk_rUnPuukiWtzOEmU7UjO8cFoPrBatCh3As=s88-c-k-c0x00ffffff-no-rj",
-    category: "Music Video",
-    description:
-      "Music video by Taylor Swift performing  “blank space” - off her 1989 album.",
-  },
-  {
-    _id: "tCXGJQYZ9JA",
-    alt: "Delicate-music-video",
-    views: "476M",
-    duration: "3:55",
-    title: "Delicate",
-    thumbnail: "/assets/gif/delicate.webp",
-    videoBy: "Taylor Swift",
-    avatar:
-      "https://yt3.ggpht.com/MqKm9xyjonzkICKA78ir0AM-WUR47ntkBeJlgHeIk_rUnPuukiWtzOEmU7UjO8cFoPrBatCh3As=s88-c-k-c0x00ffffff-no-rj",
-    category: "Music Video",
-    description:
-      "Music video by Taylor Swift performing  “delicate” - off her reputation album.",
-  },
-  {
-    _id: "tbEekLA7J3Y",
-    alt: "TheMan-music-video",
-    views: "17M",
-    duration: "3:20",
-    title: "The Man",
-    thumbnail: "/assets/gif/theman.webp",
-    videoBy: "Taylor Swift",
-    avatar:
-      "https://yt3.ggpht.com/MqKm9xyjonzkICKA78ir0AM-WUR47ntkBeJlgHeIk_rUnPuukiWtzOEmU7UjO8cFoPrBatCh3As=s88-c-k-c0x00ffffff-no-rj",
-    category: "Lyric Video",
-    description:
-      "Lyric video by Taylor Swift performing  “the man” - off her lover album.",
+      "Watch Taylor Swift play 'The Man', 'Lover', 'Death by a Thousand Cuts' and 'All Too Well' at the Tiny Desk.",
   },
   {
     _id: "wMpqCRF7TKg",
@@ -103,19 +61,105 @@ export const videos = [
       "Lyric video by Taylor Swift performing  “Champagne Problems” - off her evermore album.",
   },
   {
-    _id: "osdoLjUNFnA",
-    alt: "exile-video",
-    views: "73M",
-    duration: "4:47",
-    title: "Exile",
-    thumbnail: "/assets/gif/exile.webp",
+    _id: "RsEZmictANA",
+    alt: "willow-music-video",
+    views: "103M",
+    duration: "4:13",
+    title: "Willow",
+    thumbnail: "/assets/gif/willow.webp",
+    videoBy: "Taylor Swift",
+    avatar:
+      "https://yt3.ggpht.com/MqKm9xyjonzkICKA78ir0AM-WUR47ntkBeJlgHeIk_rUnPuukiWtzOEmU7UjO8cFoPrBatCh3As=s88-c-k-c0x00ffffff-no-rj",
+    category: "Music Video",
+    description:
+      "Official music video by Taylor Swift performing “willow” - off her evermore album.",
+  },
+  {
+    _id: "tCXGJQYZ9JA",
+    alt: "Delicate-music-video",
+    views: "476M",
+    duration: "3:55",
+    title: "Delicate",
+    thumbnail: "/assets/gif/delicate.webp",
+    videoBy: "Taylor Swift",
+    avatar:
+      "https://yt3.ggpht.com/MqKm9xyjonzkICKA78ir0AM-WUR47ntkBeJlgHeIk_rUnPuukiWtzOEmU7UjO8cFoPrBatCh3As=s88-c-k-c0x00ffffff-no-rj",
+    category: "Music Video",
+    description:
+      "Music video by Taylor Swift performing  “delicate” - off her reputation album.",
+  },
+  {
+    _id: "-BjZmE2gtdo",
+    alt: "Lover-music-video",
+    views: "189M",
+    duration: "3:59",
+    title: "Lover",
+    thumbnail: "/assets/gif/lover.webp",
+    videoBy: "Taylor Swift",
+    avatar:
+      "https://yt3.ggpht.com/MqKm9xyjonzkICKA78ir0AM-WUR47ntkBeJlgHeIk_rUnPuukiWtzOEmU7UjO8cFoPrBatCh3As=s88-c-k-c0x00ffffff-no-rj",
+    category: "Music Video",
+    description:
+      "Music video by Taylor Swift performing  “lover” - off her lover album.",
+  },
+  {
+    _id: "5UMCrq-bBCg",
+    alt: "I Bet You Think About Me-music-video",
+    views: "38M",
+    duration: "4:15",
+    title: "I Bet You Think About Me",
+    thumbnail: "/assets/gif/ibetyou.webp",
+    videoBy: "Taylor Swift",
+    avatar:
+      "https://yt3.ggpht.com/MqKm9xyjonzkICKA78ir0AM-WUR47ntkBeJlgHeIk_rUnPuukiWtzOEmU7UjO8cFoPrBatCh3As=s88-c-k-c0x00ffffff-no-rj",
+    category: "Music Video",
+    description:
+      "Music video by Taylor Swift performing I Bet You Think About Me (Taylor's Version) (From The Vault).",
+  },
+  {
+    _id: "sRxrwjOtIag",
+    alt: "all-too-well-music-video",
+    views: "53M",
+    duration: "10:13",
+    title: "All Too Well",
+    thumbnail: "/assets/gif/alltoowell.webp",
+    videoBy: "Taylor Swift",
+    avatar:
+      "https://yt3.ggpht.com/MqKm9xyjonzkICKA78ir0AM-WUR47ntkBeJlgHeIk_rUnPuukiWtzOEmU7UjO8cFoPrBatCh3As=s88-c-k-c0x00ffffff-no-rj",
+    category: "Music Video",
+    description:
+      "Official lyric video by Taylor Swift performing “All Too Well (10 Minute Version) (Taylor's Version) (From The Vault)” - off her Red (Taylor;s Version) album.",
+  },
+  {
+    _id: "e-ORhEE9VVg",
+    alt: "BlankSpace-music-video",
+    views: "2.9B",
+    duration: "4:33",
+    title: "Blank Space",
+    thumbnail: "/assets/gif/blankspace.webp",
+    videoBy: "Taylor Swift",
+    avatar:
+      "https://yt3.ggpht.com/MqKm9xyjonzkICKA78ir0AM-WUR47ntkBeJlgHeIk_rUnPuukiWtzOEmU7UjO8cFoPrBatCh3As=s88-c-k-c0x00ffffff-no-rj",
+    category: "Music Video",
+    description:
+      "Music video by Taylor Swift performing  “blank space” - off her 1989 album.",
+  },
+
+  {
+    _id: "tbEekLA7J3Y",
+    alt: "TheMan-music-video",
+    views: "17M",
+    duration: "3:20",
+    title: "The Man",
+    thumbnail: "/assets/gif/theman.webp",
     videoBy: "Taylor Swift",
     avatar:
       "https://yt3.ggpht.com/MqKm9xyjonzkICKA78ir0AM-WUR47ntkBeJlgHeIk_rUnPuukiWtzOEmU7UjO8cFoPrBatCh3As=s88-c-k-c0x00ffffff-no-rj",
     category: "Lyric Video",
     description:
-      "Lyric video by Taylor Swift performing  “exile” - off her folklore album.",
+      "Lyric video by Taylor Swift performing  “the man” - off her lover album.",
   },
+
   {
     _id: "GWtfOHBF1_w",
     alt: "you-need-to-calm-down-video",
@@ -131,18 +175,46 @@ export const videos = [
       "Lyric video by Taylor Swift performing  “You Need to Calm Down” - off her lover album.",
   },
   {
-    _id: "on4bHNdUm-I",
-    alt: "taylor-swfit-on-ellen-show",
-    views: "11M",
-    duration: "18:46",
-    title: "Taylor Swift Interview - The Ellen Show",
-    thumbnail: "/assets/gif/thellenshow.webp",
-    videoBy: "TheEllenShow",
+    _id: "osdoLjUNFnA",
+    alt: "exile-video",
+    views: "73M",
+    duration: "4:47",
+    title: "Exile",
+    thumbnail: "/assets/gif/exile.webp",
+    videoBy: "Taylor Swift",
     avatar:
-      "https://yt3.ggpht.com/I0j6g1bOYmGniEtkySB7tmvHxmo5InW1gqGRZJfFT_ErwZbRMKATBkUQyYutEPTB2A-Z2C0Z6w=s88-c-k-c0x00ffffff-no-rj",
-    category: "Interview",
+      "https://yt3.ggpht.com/MqKm9xyjonzkICKA78ir0AM-WUR47ntkBeJlgHeIk_rUnPuukiWtzOEmU7UjO8cFoPrBatCh3As=s88-c-k-c0x00ffffff-no-rj",
+    category: "Lyric Video",
     description:
-      "From singing with Zac Efron to getting scared in a bathroom, Taylor Swift has had some unforgettable moments on the show, and now she's back! The superstar opened up about the Easter eggs in her music videos and songs, her latest single, “Me!”, and preparing for the movie adaptation of the musical “Cats.” Plus, Ellen put Taylor in the hot seat to answer some of her 'Burning Questions'!",
+      "Lyric video by Taylor Swift performing  “exile” - off her folklore album.",
+  },
+  {
+    _id: "8zdg-pDF10g",
+    alt: "Paper Rings-lyric-video",
+    views: "26M",
+    duration: "3:44",
+    title: " Paper Rings",
+    thumbnail: "/assets/gif/paper.webp",
+    videoBy: "Taylor Swift",
+    avatar:
+      "https://yt3.ggpht.com/MqKm9xyjonzkICKA78ir0AM-WUR47ntkBeJlgHeIk_rUnPuukiWtzOEmU7UjO8cFoPrBatCh3As=s88-c-k-c0x00ffffff-no-rj",
+    category: "Lyric Video",
+    description:
+      "Official audio for 'Paper Rings' performed by Taylor Swift. Off her new album 'Lover.' ",
+  },
+  {
+    _id: "nn_0zPAfyo8",
+    alt: "evermore-lyric-video",
+    views: "40M",
+    duration: "4:24",
+    title: "August",
+    thumbnail: "/assets/gif/august.webp",
+    videoBy: "Taylor Swift",
+    avatar:
+      "https://yt3.ggpht.com/MqKm9xyjonzkICKA78ir0AM-WUR47ntkBeJlgHeIk_rUnPuukiWtzOEmU7UjO8cFoPrBatCh3As=s88-c-k-c0x00ffffff-no-rj",
+    category: "Lyric Video",
+    description:
+      "Official lyric video by Taylor Swift performing 'august' - off her album 'folklore.'",
   },
   {
     _id: "0Kr4JO9591c",
@@ -173,17 +245,17 @@ export const videos = [
       "Taylor Swift -  cardigan / august / willow (Live at the GRAMMYs 2021)",
   },
   {
-    _id: "FvVnP8G6ITs",
-    alt: "taylor-swift-npr-performance",
-    views: "10M",
-    duration: "28:59",
-    title: "Taylor Swift- NPR Music Tiny Desk Concert",
-    thumbnail: "/assets/gif/npr.webp",
-    videoBy: "NPR Music",
+    _id: "on4bHNdUm-I",
+    alt: "taylor-swfit-on-ellen-show",
+    views: "11M",
+    duration: "18:46",
+    title: "Taylor Swift Interview - The Ellen Show",
+    thumbnail: "/assets/gif/thellenshow.webp",
+    videoBy: "TheEllenShow",
     avatar:
-      "https://yt3.ggpht.com/ytc/AKedOLTsXk5nEOKKabk8BITs9DYeYR09vM0vORF7c37bEA=s88-c-k-c0x00ffffff-no-rj",
-    category: "Concert",
+      "https://yt3.ggpht.com/I0j6g1bOYmGniEtkySB7tmvHxmo5InW1gqGRZJfFT_ErwZbRMKATBkUQyYutEPTB2A-Z2C0Z6w=s88-c-k-c0x00ffffff-no-rj",
+    category: "Interview",
     description:
-      "Watch Taylor Swift play 'The Man', 'Lover', 'Death by a Thousand Cuts' and 'All Too Well' at the Tiny Desk.",
+      "From singing with Zac Efron to getting scared in a bathroom, Taylor Swift has had some unforgettable moments on the show, and now she's back! The superstar opened up about the Easter eggs in her music videos and songs, her latest single, “Me!”, and preparing for the movie adaptation of the musical “Cats.” Plus, Ellen put Taylor in the hot seat to answer some of her 'Burning Questions'!",
   },
 ];
